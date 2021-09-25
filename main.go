8bb95ef9047745b13e15b1c3f616dbf0a8d3d79b
Max Santos <maxedwardsantos@gmail.com>
2021-09-25 16:44:39 -0400

    Refactoring

--- main.go
+++ main.go
@@ -1,36 +1,21 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
-	"os/exec"
-	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/santosemax/ReAL/handlers"
 )
 
 var port string = ":8080"
 var templates = template.Must(template.ParseFiles("templates/index.html"))
 
-// Overall Page Data
-type Page struct {
-	Title       string
-	PageResults []Results
-}
-
-// Storing Results
-type Results struct {
-	ResultTitle string
-	ResultBody  string
-}
-
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/process", searchHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/process", handlers.SearchHandler)
 
 	// Handle Static Files
 	http.Handle("/static/images/", http.StripPrefix("/static/images/", http.FileServer(http.Dir("./static/images"))))
@@ -39,106 +24,4 @@ func main() {
 	fmt.Printf("Opening Server on Port %s:\n", port[1:5])
 	log.Fatal(http.ListenAndServe(port, nil))
 
-}
-
-// handles home page
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		// Redirect if not a real page
-		http.Redirect(w, r, "/", http.StatusFound)
-		fmt.Fprintf(w, "Error")
-	}
-	t.Execute(w, t)
-}
-
-// handles search results (NEED TO GET VARIABLES TO PAGE USING EXECUTE)
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-
-	// Render Page
-	t, err := template.ParseFiles("templates/results.html")
-	if err != nil {
-		// Redirect if not a real page
-		http.Redirect(w, r, "/", http.StatusFound)
-		fmt.Fprintf(w, "Error")
-	}
-
-	var val string
-	var page Page
-	var results []Results
-
-	// Initialize DB
-	db, err := sql.Open("sqlite3", "./results.db")
-	checkErr(err)
-
-	// Create Tables (if needed)
-	statement, _ := db.Prepare("CREATE TABLE IF NOT EXISTS queryQ (query text)")
-	statement.Exec()
-	statement, _ = db.Prepare("CREATE TABLE IF NOT EXISTS results (title text, body text)")
-	statement.Exec()
-
-	fmt.Println("method:", r.Method) // console shows method
-	if r.Method != "GET" {
-		t, _ := template.ParseFiles("templates/whoops.html")
-		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-		for key, value := range r.Form {
-			fmt.Printf("%s = %s\n", key, value)
-
-			// Put query request into queryQ table
-			val = strings.Join(value, "")
-			statement, err = db.Prepare("INSERT INTO queryQ (query) VALUES (?)")
-			statement.Exec(fmt.Sprint(val))
-			checkErr(err)
-			statement.Close()
-		}
-		// SEARCH LOGIC STARTS HERE (Python->DB->GO)
-
-		// Run Python Scraper (REDDIT)
-		cmd := exec.Command("./web/scrape.py")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		log.Println(cmd.Run())
-
-		// DB STUFF
-		// Read Result.db's temporary data from Python File
-		rows, err := db.Query("SELECT * FROM results")
-		checkErr(err)
-		var title string
-		var body string
-
-		// Read the Rows
-		for rows.Next() {
-
-			// Read rows and output to html page
-			err := rows.Scan(&title, &body)
-			checkErr(err)
-
-			results = append(results, Results{ResultTitle: title, ResultBody: body})
-
-		}
-		rows.Close()
-
-		// Delete Rows and close db
-		db.Exec("DELETE FROM results")
-		db.Exec("DELETE FROM queryQ")
-
-		db.Close()
-	}
-
-	// Fill out Page
-	page.Title = val
-	page.PageResults = results
-
-	// EXECUTE TEMPLATE HERE WITH STATIC VARIABLES
-	t.Execute(w, page)
-
-}
-
-// Check DB for errors
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
