9059701a6ccba53b2cd0dbb18634e88b2e48d66e
Max Santos <maxedwardsantos@gmail.com>
2021-10-04 23:08:46 -0400

    Can't scrape must use api

--- web/stack.py
+++ web/stack.py
@@ -1,30 +1,37 @@
 #!/usr/local/bin/python3
+from stackapi import StackAPI
+from datetime import datetime
 
-#from stackapi import StackAPI
-#from datetime import datetime
-#SITE = StackAPI('askubuntu')
+
+
+
+query = "How to turn on computer"
+SITE = StackAPI('stackoverflow')
+
+# Parameters for search
+SITE.page_size = 10
+SITE.max_pages = 1
+
+
+# Get search 
+questions = SITE.fetch('search/advanced', title=query)
+
+print(questions)
+
+for q in questions:
+    print(q)
+
+
 #
+#import requests
+#from bs4 import BeautifulSoup
 #
-## Parameters for search
-#SITE.page_size = 10
-#SITE.max_pages = 1
+#res = requests.get("https://stackoverflow.com/search?q=How+to+turn+off+the+computer")
+#soup = BeautifulSoup(res.text, "html.parser")
+#questions = soup.select(".question-summary")
 #
+## Print out questions
+#for que in questions:
+#    q = que.select_one('.question-hyperlink').getText()
+#    print(q)
 #
-## Get search 
-#query = SITE.fetch('questions', fromdate=datetime(2020, 1, 1), todate=datetime(2021, 11, 3), min=10, sort='votes')
-#comments = SITE.fetch('comments')
-#
-#print(query)
-
-
-import requests
-from bs4 import BeautifulSoup
-
-res = requests.get("https://stackoverflow.com/questions")
-
-soup = BeautifulSoup(res.text, "html.parser")
-
-
-questions = soup.select(".question-summary")
-
-print(questions[0].attrs)
