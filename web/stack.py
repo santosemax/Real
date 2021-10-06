a8b46ea360e3373fa7b7a09ee5d43b9a0919dce1
Max Santos <maxedwardsantos@gmail.com>
2021-10-06 19:39:09 -0400

    stackapi is hard

--- web/stack.py
+++ web/stack.py
@@ -2,36 +2,15 @@
 from stackapi import StackAPI
 from datetime import datetime
 
+query = "How to include python in golang"
 
-
-
-query = "How to turn on computer"
 SITE = StackAPI('stackoverflow')
 
 # Parameters for search
-SITE.page_size = 10
-SITE.max_pages = 1
-
+#SITE.page_size = 10
+#SITE.max_pages = 10
 
 # Get search 
-questions = SITE.fetch('search/advanced', title=query)
+questions = SITE.fetch('similar', title='test')
 
 print(questions)
-
-for q in questions:
-    print(q)
-
-
-#
-#import requests
-#from bs4 import BeautifulSoup
-#
-#res = requests.get("https://stackoverflow.com/search?q=How+to+turn+off+the+computer")
-#soup = BeautifulSoup(res.text, "html.parser")
-#questions = soup.select(".question-summary")
-#
-## Print out questions
-#for que in questions:
-#    q = que.select_one('.question-hyperlink').getText()
-#    print(q)
-#
